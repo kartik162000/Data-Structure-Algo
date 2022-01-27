a18283ce7db484e6b157983b21aaf3233d32aa31
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-01-27 20:48:44 +0530

    Time: 70 ms (26.22%), Space: 20.9 MB (27.47%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -7,10 +7,7 @@ public:
         {
             string ori=x;
             sort(x.begin(),x.end());
-            if(m.find(x)==m.end())
-                m[x].push_back(ori);
-            else
-                m[x].push_back(ori);
+            m[x].push_back(ori);
         }
         int i=0;
         for(auto x:m)
