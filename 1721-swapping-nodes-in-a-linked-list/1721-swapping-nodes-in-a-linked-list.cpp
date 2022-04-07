b2356f4cacd18d1d51fe736015f633073bea17a7
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-04-07 11:43:20 +0530

    Time: 838 ms (59.52%), Space: 180.2 MB (57.21%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -22,30 +22,17 @@ public:
             prev=temp;
             temp=temp->next;
         }
-        if(len<2)
-            return head;
-        if(len==k || k==1)
-        {
-            swap(prev->val,head->val);
-            return head;
-        } 
         int ct=0;
         ListNode *t,*t1;
         while(ct<len)
         {
             if(ct==k-1)
-            {
                t=l;
-            }
            if(ct==len-k)
-           {
                t1=l;
-           }
             ct++;
             l=l->next;
         }
-        cout<<"left value is:"<<t->val<<"\n";
-        cout<<"right value is:"<<t1->val<<"\n";
         swap(t->val,t1->val);
         return head;
     }
