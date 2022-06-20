ae351fb9319020f160ff748ee8d94f0bcc9f9fac
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-06-20 13:39:08 +0530

    Time: 16 ms (88.84%), Space: 16.4 MB (96.60%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int ans=0;
-        int n=nums.size();
-        int curr=0,jump=0;
-        for(int i=0;i<n-1;i++){
-            jump=max(jump,i+nums[i]);
-            if(i==curr){
-                ans++;
-                curr=jump;
-            }
-        }
-        return ans;
-    }
+        
+        int max_reach=0;
+        int current=0;
+        int jump=0;
+       for(int i=0;i<nums.size()-1;i++)
+       {
+           max_reach=max(max_reach,nums[i]+i);
+           if(i==current)
+           {
+               current=max_reach;
+               jump++;
+           }
+       }
+        return jump;
+}
 };
