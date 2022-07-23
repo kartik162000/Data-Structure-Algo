f64e722da120d1d9dc17f152f3d06cadaa5429f7
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-07-23 23:46:03 +0530

    Time: 0 ms (100.00%), Space: 8 MB (37.41%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void help(vector<vector<int>>&vec,vector<int> &nums,int index)
+    vector<vector<int>> vec;
+    void help(vector<int>&nums,int index)
     {
         if(index==nums.size())
         {
-             vec.push_back(nums);
-              return;
+            vec.push_back(nums);
+            return;
         }
-          
-        for(int i=index;i<nums.size();i++)
-        {
-            swap(nums[i],nums[index]);
-            help(vec,nums,index+1);
-            swap(nums[i],nums[index]);
-        }
-        return;
+            for(int i=index;i<nums.size();i++)
+            {
+                swap(nums[i],nums[index]);
+                help(nums,index+1);
+                swap(nums[i],nums[index]);
+            }
+            
     }
     vector<vector<int>> permute(vector<int>& nums) {
-    vector<vector<int>> vec;
-    help(vec,nums,0);
-     return vec;   
+    help(nums,0);
+        return vec;
     }
 };
-
-
-
-/*  stl trick function next_permuation(nums.begin(),nums.end()) */
