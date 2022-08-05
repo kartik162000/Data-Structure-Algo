2dae6c18cc72c18834c2db190c185d8901a5dd49
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-08-05 23:33:20 +0530

    Time: 4 ms (97.86%), Space: 7.3 MB (84.10%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
