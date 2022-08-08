9fec43a5f1bf6e68b6441a5126c4a98131d6467c
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-08-08 07:16:13 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -16,7 +16,7 @@ public:
     }
     int combinationSum4(vector<int>& nums, int target) {  
         vector<int>dp(target+1,-1);
-        dp[0]=0;
+        dp[0]=1;
         return solve(nums,target,dp);
     }
 };
