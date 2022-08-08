daf531bc6ec1a510c49907801ce440dd877e57d2
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-08-08 16:21:41 +0530

    Time: 4 ms (36.13%), Space: 7.7 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,76 +1,17 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) 
-    {
-        
-        vector<int> dp(nums.size(),0);
-        int n=nums.size();
-        if(n==0)
-            return 0;
-        dp[0]=nums[0];
-        if(n==1)
-            return dp[0];
-        dp[1]=max(nums[0],nums[1]);
-        if(n==2)
-            return dp[1];
-        for(int i=2;i<n;i++)
-        {
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[n-1];
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         vector<int> a(nums.size());
-//         int n=nums.size();
-//         if(n==0)
-//             return 0;
-//              a[0]=nums[0];
-//        if(n==1)
-//        {
-//            return a[0];
-//        }
-//         a[1]=max(nums[0],nums[1]);
-//         if(n==2)
-//             return a[1];
-        
-//         for(int i=2;i<n;i++)
-//         {
-//             a[i]=max(a[i-1],nums[i]+a[i-2]);
-//         }
-//         return a[n-1];
+    int rob(vector<int>& houses) {
+    vector<int>dp(houses.size(),-1);
+         if(houses.size()==1)
+         return houses[0];
+         dp[0]=houses[0];
+     if(houses.size()==2)
+         return max(houses[0],houses[1]);
+     dp[1]= max(houses[0],houses[1]);
+     for(int i=2;i<houses.size();i++)
+     {
+         dp[i]=max(houses[i]+dp[i-2],dp[i-1]);
+     }
+     return dp[houses.size()-1];
     }
-     
-       
-        
-        
 };
