38031ac2d956b5ba5ff58902df228f797c373e5d
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2022-08-11 00:41:43 +0530

    Time: 0 ms (100.00%), Space: 7 MB (87.43%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void solve(vector<int>& nums,vector<int>&v,vector<vector<int>>&vec,int index)
+    void solve(vector<int>&v,vector<vector<int>> &vec,int index,vector<int> &nums)
     {
         vec.push_back(v);
         for(int i=index;i<nums.size();i++)
         {
             v.push_back(nums[i]);
-            solve(nums,v,vec,i+1);
+            solve(v,vec,i+1,nums);
             v.pop_back();
         }
         return;
     }
-    
-    
-    vector<vector<int>> subsets(vector<int>& nums) { 
+    vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> vec;
         vector<int> v;
-        solve(nums,v,vec,0);
+        solve(v,vec,0,nums);
         return vec;
     }
 };
